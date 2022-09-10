53c6326cb266266ccb57ba92fe2121d20fe40782
The Best:) <55598887+PaulMoroz@users.noreply.github.com>
2022-09-11 01:07:31 +0300

    Removed all unused code

--- app.py
+++ app.py
@@ -1,45 +1,18 @@
 from flask import Flask, render_template, request, jsonify, make_response
 from dbsetup import create_connection, select_all_items, update_item
 from flask_cors import CORS, cross_origin
-from pusher import Pusher
 import simplejson
 import os
 
 app = Flask(__name__)
 cors = CORS(app)
 
-pusher = Pusher(app_id=os.environ.get('PUSHER_ID'),
-  key=os.environ.get('PUSHER_KEY'),
-  secret=os.environ.get('PUSHER_SECRET'),
-  cluster='eu')
-database = "./pythonsqlite.db"
-conn = create_connection(database)
-c = conn.cursor()
-
-
-def main():
-	global conn, c
-
 
 @app.route('/')
-def index():
-	return render_template('index.html')
+def hello_world():  # put application's code here
+    return 'Hello World!'
 
-
-@app.route('/admin')
-def admin():
-	return render_template('admin.html')
-
-
-@app.route('/vote', methods=['POST'])
-def vote():
-	data = simplejson.loads(request.data)
-	update_item(c, [data['member']])
-	output = select_all_items(c, [data['member']])
-	pusher.trigger(u'poll', u'vote', output)
-	return request.data
 
 
 if __name__ == '__main__':
-	main()
 	app.run(debug=True)
